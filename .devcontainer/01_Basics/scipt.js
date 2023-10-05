429d2e613ede483ef935916224e656d42ae49582
Pradumn Dubey <140749011+dubeypradum999@users.noreply.github.com>
2023-10-05 21:30:52 +0000

    container config

--- .devcontainer/01_Basics/scipt.js
+++ .devcontainer/01_Basics/scipt.js
@@ -1,1 +1,9 @@
-console.log("Pradumn")
+const accountId = 123456
+let accountEmail = "dubey399@gmail.com"
+var accountPassword = "56789"
+accountCity = "Jaipur"
+
+// accountId = 2
+ 
+
+console.log([accountId , accountEmail, accountPassword , accountCity]);
